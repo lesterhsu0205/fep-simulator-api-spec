94001be2ab931f6907f9308e439b39c9147a90c1
Lester Hsu <lesterhsu0205@hotmail.com>
2025-08-29 10:42:22 +0800

    reset yaml

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var options = {
     },
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/api-specification.yaml'), options);
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
@@ -21,4 +21,3 @@ http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
-
